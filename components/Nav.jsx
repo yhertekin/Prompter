323e0ce168d9fa0ba35276e72dfcec10e901955c
yhertekin <yhertekin@gmail.com>
2023-09-17 21:23:11 +0300

    update

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,7 +6,7 @@ import { useState, useEffect } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
-    const isUserLoggedIn = true;
+    const { data: session } = useSession();
     const [providers, setProviders] = useState(null);
     const [toggleDropdown, setToggleDropdown] = useState(false);
 
@@ -30,7 +30,7 @@ const Nav = () => {
                 <p className='logo_text'>Prompter</p>
             </Link>
             <div className='sm:flex hidden'>
-                {isUserLoggedIn ? (
+                {session?.user ? (
                     <div className='flex gap-3 md:gap-5'>
                         <Link className='black_btn' href='/create-prompt'>
                             Create Post
@@ -40,7 +40,7 @@ const Nav = () => {
                         </button>
                         <Link href='/profile'>
                             <Image
-                                src='/assets/images/logo.svg'
+                                src={session?.user.image}
                                 width={37}
                                 height={37}
                                 className='rounded-full'
@@ -65,10 +65,10 @@ const Nav = () => {
                 )}
             </div>
             <div className='sm:hidden flex relative'>
-                {isUserLoggedIn ? (
+                {session?.user ? (
                     <div className='flex'>
                         <Image
-                            src='/assets/images/logo.svg'
+                            src={session?.user.image}
                             width={37}
                             height={37}
                             className='rounded-full'
---
+++ models/user.js
@@ -0,0 +1,19 @@
+import { Schema, model, models } from "mongoose";
+
+const UserSchema = new Schema({
+    email: {
+        type: String,
+        unique: [true, "Email already exist!"],
+        required: [true, "Email is required!"],
+    },
+    username: {
+        type: String,
+        required: [true, "User name is required!"],
+    },
+    image: {
+        type: String,
+    },
+});
+
+const User = models.User || model("User", UserSchema);
+export default User;
